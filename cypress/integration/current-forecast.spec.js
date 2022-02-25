168581c9492234f5b56397ec64d44a37999f51a3
James York <kroysemaj@gmail.com>
2022-02-25 17:35:28 -0500

    extract api key, refactor e2e test inputs

--- cypress/integration/current-forecast.spec.js
+++ cypress/integration/current-forecast.spec.js
@@ -1,13 +1,10 @@
 import { currentMockData } from '../fixtures/current';
+import API_KEY from '../../src/apiKey';
 
 describe('Getting Current forecast', () => {
-  const API_KEY = 'bd9fd5f9e16dae5895c5d1b1992f5ba7';
-  const DetroitCoords = { lat: 42.33143, long: -83.0458 };
-  const responseCityName = 'Detroit';
-
   it('should show weather for a given lat/long', () => {
-    const cityName = 'Detroit';
-
+    const DetroitCoords = { lat: 42.33143, long: -83.0458 };
+    const responseCityName = 'Detroit';
     cy.server({ force404: true });
 
     cy.route({
---
+++ src/apiKey.js
@@ -0,0 +1,3 @@
+const API_KEY = 'bd9fd5f9e16dae5895c5d1b1992f5ba7';
+
+export default API_KEY;
