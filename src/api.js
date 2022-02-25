abd7cb57494ca47c420dbdc9b6bcd962a1c84c84
James York <kroysemaj@gmail.com>
2022-02-25 17:50:34 -0500

    WIP - need lat/long. See todos in api.js

--- src/api.js
+++ src/api.js
@@ -5,6 +5,13 @@ const client = axios.create({
   baseURL: 'https://api.openweathermap.org/data/2.5/',
 });
 
+/*
+ *  TODO:
+ * - get lat/long from geolocation api
+ * - add lat/long to state
+ * - use lat/long from state to load forecast
+ */
+
 const api = {
   loadCurrentForecast(lat, long) {
     const params = {
