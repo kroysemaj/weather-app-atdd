645e5757de8495a6c404ba123105cb0e2cd8399a
James York <kroysemaj@gmail.com>
2022-03-03 16:41:10 -0500

    update updated name

--- cypress/integration/current-forecast.spec.js
+++ cypress/integration/current-forecast.spec.js
@@ -1,16 +1,16 @@
-import { currentMockData } from '../fixtures/current';
+import { currentApiMockData } from '../fixtures/current';
 import API_KEY from '../../src/apiKey';
 
 describe('Getting Current forecast', () => {
   it('should show weather for a given lat/long', () => {
     const DetroitCoords = { lat: 42.33143, long: -83.0458 };
     const responseCityName = 'Detroit';
-    cy.server({ force404: true });
+    cy.server({ force404: false });
 
     cy.route({
       method: 'GET',
       url: `http://api.openweathermap.org/data/2.5/weather?lat=${DetroitCoords.lat}&lon=${DetroitCoords.long}&appid=${API_KEY}`,
-      response: currentMockData,
+      response: currentApiMockData,
     });
 
     cy.visit('/');
