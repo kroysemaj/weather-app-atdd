5463f00d667b64c46cc5b53808fc9b68ec1b7820
James York <kroysemaj@gmail.com>
2022-03-03 16:41:21 -0500

    notes for myself

--- src/models/applicationState.js
+++ src/models/applicationState.js
@@ -4,16 +4,20 @@
  */
 
 const initialApplicationState = {
+  // comes from weather api /weather
   current: {
     high: 0,
     low: 0,
     icon: '',
   },
+  // comes from API /weather response
   city: '',
+  //comes from browser geolocation or user input (city or zip)
   coords: {
     lon: 0,
     lat: 0,
   },
+  // comes from weather api /fiveday
   fiveDay: {},
 };
 
