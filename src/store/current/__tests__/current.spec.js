a5b3d24dd10876786325942fae53daed04d94a65
James York <kroysemaj@gmail.com>
2022-03-03 16:42:03 -0500

    save a subset of the api response to data store

--- src/store/current/__tests__/current.spec.js
+++ src/store/current/__tests__/current.spec.js
@@ -7,16 +7,30 @@ import initialState from '../../../models/applicationState';
 describe('Current forecast store', () => {
   describe('loadCurrentForecast action', () => {
     it('stores the current forecast', async () => {
-      const expected = {
-        current: {
-          high: 80,
-          low: 78,
-          icon: 'sunny',
+      const response = {
+        main: {
+          temp_max: 80,
+          temp_min: 78,
+          feels_like: 76,
         },
+        weather: [
+          {
+            icon: 'sunny',
+          },
+        ],
+        name: 'Detroit',
+      };
+
+      const expected = {
+        high: 80,
+        low: 78,
+        feelsLike: 76,
+        icon: 'sunny',
+        name: 'Detroit',
       };
 
       const api = {
-        loadCurrentForecast: () => Promise.resolve(expected),
+        loadCurrentForecast: () => Promise.resolve(response),
       };
 
       const store = createStore(
--- src/store/current/actions.js
+++ src/store/current/actions.js
@@ -1,7 +1,14 @@
 export const STORE_CURRENT_FORECAST = 'STORE_CURRENT_FORECAST';
 
 export const loadCurrentForecast = () => (dispatch, getState, api) => {
-  api.loadCurrentForecast().then(current => {
+  api.loadCurrentForecast().then(response => {
+    const current = {
+      high: response.main.temp_max,
+      low: response.main.temp_min,
+      feelsLike: response.main.feels_like,
+      icon: response.weather[0].icon,
+      name: response.name,
+    };
     dispatch(storeCurrentForecast(current));
   });
 };
